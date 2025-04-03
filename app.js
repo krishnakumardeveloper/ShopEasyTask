762f4637342f0dd9bfc5296277531140d8017879
krishnakumardeveloper <krishnakumarwebdeveloper@gmail.com>
2025-04-03 14:39:29 +0530

    App.js Modified : Message change

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get("/", (req,res)=>{
-    res.send("welcome to ShopEasy")
+    res.send("welcome to ShopEasy From Kriz Code !")
 })
 
 app.listen(PORT, ()=>{
